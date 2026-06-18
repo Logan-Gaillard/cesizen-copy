823a1b29fd1a155d012e1bc6d20275eb21998c94
Logan-Gaillard <logangaillard1@gmail.com>
2026-06-18 10:27:42 +0200

    chore(ci): configure husky et commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"scope-enum": [
+			2,
+			"always",
+			[
+				"auth", // actions/user.ts, login/register/profile
+				"infos", // actions/information.ts, app/actus, app/actu
+				"respiration", // app/exercices (exercices de respiration)
+				"admin", // app/admin
+				"ui", // composants partagés Flex, Title, Form*
+				"db", // prisma/schema.prisma, migrations
+				"ci", // pipeline CI/CD
+				"docker", // Dockerfile, docker-compose
+				"iac", // Pulumi
+				"deps", // dépendances
+			],
+		],
+	},
+};
